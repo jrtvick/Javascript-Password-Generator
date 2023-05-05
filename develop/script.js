585536213e3ff7ac70733e361631b749c4ab16f4
Tyler Vick <jrtvick@gmail.com>
2023-05-04 21:07:54 -0400

    adding to the ReadMe file

--- develop/script.js
+++ develop/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code part 1 of 2
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -16,29 +16,34 @@ var uppercaseABC = ["A","B","C","D",'E','F','G','H','I','J','K','L','M','N','O',
 var lowercaseABC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialSymbols = ["!","@","#","$","%","^","&","*","(",")","?","/","<",">",",",".",":",";"];
 var numeric = ["0","1","2","3","4","5","6","7","8","9"];
-var combinedChars = [];
-var pwString = "" ;
-
 
 // This function is the crux of the password generation, all of the action happens in here.
 function generatePassword() {
+
 // Setting up all the variables to meet the criteria.
-  var uppercase = confirm("For your safety, your password should have an uppercase letter! \nClick OK to continue");
-  var lowercase = confirm("For your safety, your password should have a lowercase letter! \nClick OK to continue");
-  var symbols = confirm("For your safety, your password should have a symbol! \nClick OK to continue");
-  var numbers = confirm("For your safety, your password should have a number! \nClick OK to continue");
-  var keyLength = prompt("Password must be between 8 and 128 characters! \nClick OK to continue");
-// Establishing an if statement to check for password selections. If nothing is selected, the entry is invalid.
+  var uppercase = confirm("For your safety, your password should have an uppercase letter. \nClick OK to continue.");
+  var lowercase = confirm("For your safety, your password should have a lowercase letter. \nClick OK to continue.");
+  var symbols = confirm("For your safety, your password should have a symbol. \nClick OK to continue.");
+  var numbers = confirm("For your safety, your password should have a number. \nClick OK to continue.");
+  var keyLength = prompt("Password must be between 8 and 128 characters. \nClick OK to continue.");
+
+// These variables play a role in generating the password in my math section lower down.
+  var combinedChars = [];
+  var pwString = "";
+
+// Establishing a nested if statement to check for password selections. If nothing is selected, the entry is invalid.
   if (uppercase === false && lowercase === false && numbers === false && symbols === false) {
     alert("Invalid entry. At least one option must be selected.");
     return pwString;
   } 
-// Making sure the user applies the correct password length & format
+
+// Using an additional nested if statement that ensures the user applies the correct password length & format.
   if (keyLength < 8 || keyLength > 128) {
-    alert("Your password does not meet the criteria");
+    alert("Your selection must be between 8 and 128 characters.");
     var keyLength = prompt("Password must be between 8 and 128 characters in length.");
   }
-// These if statements concatinate the user's selections from the confirmations and prompt together.
+
+// These nested if statements concatinate the user's selections from the confirmations and prompt together.
   if (uppercase === true) {
     combinedChars = combinedChars.concat(uppercaseABC);
    }
@@ -51,17 +56,19 @@ function generatePassword() {
   if (numbers === true) {
     combinedChars = combinedChars.concat(numeric);
    }
-   // 
+
+// This for loop accomplishes the math portion of my password generation. It takes the information from the keyLength, pwString, and combinedChars variables and returns a randomized combination to the user.
   for(var i = 0; i < keyLength; i++) {
     var mathFloor = Math.floor(Math.random() * combinedChars.length);
     pwString = pwString + combinedChars[mathFloor];
    }
   return pwString; 
 }
-// My code ends here
+
+// My code ends here!
 
 
-// Assignment Code
+// Assignment Code part 2 of 2
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
